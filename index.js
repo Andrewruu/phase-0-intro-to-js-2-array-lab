17f380aa9a5474272802c42a034ba713d36518cc
Andrew Wang <Andrewruujin@gmail.com>
2022-08-22 16:16:59 -0400

    commit tree 856a380bde5b57d97d0b763b247d82e860687102

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(name){
+    const newcats = [...cats, name];
+    return newcats;
+}
+
+function prependCat(name){
+    const newcats = [name,...cats];
+    return newcats;
+}
+
+function  removeLastCat(){
+    const newcats = [...cats.slice(0,2)];
+    return newcats;
+}
+
+function removeFirstCat(){
+    const newcats = [...cats.slice(1,3)];
+    return newcats;
+}
